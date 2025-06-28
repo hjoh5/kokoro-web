df51dd455e8a18239d3513bcba35dbbc52d8a95b
hjoh5 <bdfghdfr4@outlook.sa>
2025-06-28 03:37:31 +0300

    fix: use Vercel adapter instead of Node adapter
    Changed adapter to make it work on Vercel hosting.

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,12 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
-import adapterNode from "@sveltejs/adapter-node";
-import adapterStatic from "@sveltejs/adapter-static";
-
-// https://svelte.dev/docs/kit/adapter-node
-let adapter = adapterNode();
-
-// https://svelte.dev/docs/kit/adapter-static
-if (process.env.ADAPTER === "static") {
-  adapter = adapterStatic({
-    strict: false,
-    fallback: "index.html",
-  });
-}
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter,
-
-    // See https://svelte.dev/docs/kit/configuration#env
+    adapter: adapter(),
     env: {
       privatePrefix: "KW_SECRET_",
       publicPrefix: "KW_PUBLIC_",
